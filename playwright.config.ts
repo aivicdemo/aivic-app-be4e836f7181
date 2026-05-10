67a31f8dd043f2d1f02594aa1c19bee3316373d1
aivicdemo <aivicdemo@gmail.com>
2026-05-10 20:07:06 +0900

    chore: refresh AIVIC managed test config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests/e2e",
   use: { baseURL: process.env.PLAYWRIGHT_BASE_URL || "https://dev.d3r3rai463jny7.amplifyapp.com" },
+  workers: process.env.CI ? 6 : undefined,
+  timeout: 30_000,
   reporter: "list",
 });
